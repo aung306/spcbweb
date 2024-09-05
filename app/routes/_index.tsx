d7c790ce6e1790bb4a386b3a9561adeb480c3a75
Angela Ung <angelaung@ufl.edu>
2024-09-05 11:42:38 -0400

    install css animations

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -9,13 +9,13 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div className="font-sans p-4">
-      <h1 className="text-3xl">Welcome to the Society of PC Building Website!</h1>
-      <a className="text-2xl">Here are some links to help you get started on building a website on Remix! Reach out to e-board for any questions.</a>
-      <ul className="list-disc mt-4 pl-6 space-y-2">
+    <div className="animate-fade-up m-20 rounded-md font-mono p-4 justify-center border border-grey-300">
+      <h1 className="animate-fade-up text-3xl">Welcome to the Society of PC Building Website!</h1>
+      <h2 className="animate-fade-up text-xl">Here are some links to help you get started on building a website on Remix! Reach out to e-board for any questions.</h2>
+      <ul className="animate-fade-up list-disc mt-4 pl-6 space-y-2">
         <li>
           <a
-            className="text-blue-700 underline visited:text-purple-900"
+            className="text-black underline visited:text-purple-900"
             target="_blank"
             href="https://remix.run/start/quickstart"
             rel="noreferrer"
